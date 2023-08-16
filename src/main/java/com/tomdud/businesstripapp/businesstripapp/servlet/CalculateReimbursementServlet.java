e3e64e94edb529d36b2abda3e7bb5f79fdde895a
tomdud-developer <tomadud045@student.polsl.pl>
2023-08-16 21:57:24 +0200

    Developing reimbursement-calculator form view :: add start trip date field

---
+++ src/main/java/com/tomdud/businesstripapp/businesstripapp/servlet/CalculateReimbursementServlet.java
@@ -0,0 +1,25 @@
+package com.tomdud.businesstripapp.businesstripapp.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@WebServlet(name = "CalculateReimbursementServlet", value = "/calculate-reimbursement")
+public class CalculateReimbursementServlet extends HttpServlet {
+
+    private static final Logger logger = Logger.getLogger(CalculateReimbursementServlet.class.getName());
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.info("Receive post request");
+    }
+
+}
---
+++ src/main/webapp/reimbursement-calculator.jsp
@@ -0,0 +1,18 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Business Trip Reimbursement Calculation Page</title>
+</head>
+<body>
+    <h1>Business Trip Reimbursement Calculator</h1>
+    <form method="post" action="calculate-reimbursement">
+        <h3>Provide data:</h3>
+        <br>
+        <label for="tripStartDate">Trip date:</label>
+        <input type="date" id="tripStartDate" name="tripStartDate">
+
+
+        <input type="submit">
+    </form>
+</body>
+</html>
