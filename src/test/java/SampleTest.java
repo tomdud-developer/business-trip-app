a65cbb85b825c616fb1d17bedf780ab7a87738ae
tomdud-developer <tomadud045@student.polsl.pl>
2023-08-16 20:37:49 +0200

    Write sample test

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+
+    @Test
+    void sampleTest() {
+        //Run sample test
+        Assertions.assertTrue(true);
+    }
+}
