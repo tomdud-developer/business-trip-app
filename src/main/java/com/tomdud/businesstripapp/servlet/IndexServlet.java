c8ad69d20e8aff3cfb687274848a73584b8a222e
tomdud-developer <tomadud045@student.polsl.pl>
2023-08-20 22:26:08 +0200

    Add IndexServlet to redirect from / to /dashboard

---
+++ src/main/java/com/tomdud/businesstripapp/servlet/IndexServlet.java
@@ -0,0 +1,15 @@
+package com.tomdud.businesstripapp.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "IndexServlet", value = "/")
+public class IndexServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.sendRedirect(request.getContextPath() + "/dashboard");
+    }
+}
